dfd3123a354b8637e0c55f50a3c7d998649bd3a7
RuWe <Ruthweber95@web.de>
2016-06-02 13:45:52 +0200

    Smiley hinzugefügt

---
+++ Smiley.asm
@@ -0,0 +1,44 @@
+;--------------------------------------------------------------------
+;Modul - Smiley
+;--------------------------------------------------------------------
+ljmp INIT
+
+ORG 00H
+
+INIT:
+mov R0,#10011001b
+mov R1,#11111001b
+mov R2,#10111101b
+mov R3,#10000001b
+call lachen
+
+;Lachen
+LACHEN:
+mov P0,R0
+mov P1,R1
+;mov P1,#11111011b
+mov P0,#11111111b
+mov P0,R2
+mov P1,#11011111b
+mov P1,#10111111b
+mov P0,R3
+
+jmp zwinkern
+;call traurig
+
+ZWINKERN:
+mov P0,R0
+mov P1,R1
+mov P1,#11111011b
+mov P0,#11111111b
+mov P0,R2
+mov P1,#11011111b
+mov P1,#10111111b
+mov P0,R3
+;jmp lachen
+call lachen
+
+
+end
+;------- Smiley Ende------------------------------------------------
+
